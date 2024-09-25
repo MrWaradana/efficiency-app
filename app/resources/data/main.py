dd3d38152421db2b64f23e6f1f1462e5090d1355
Cizz22 <cisatraa@gmail.com>
2024-09-25 11:40:55 +0700

    fix bug

--- app/resources/data/main.py
+++ app/resources/data/main.py
@@ -3,7 +3,9 @@ from datetime import datetime
 import requests
 from digital_twin_migration.database import Propagation, Transactional
 from digital_twin_migration.models.efficiency_app import (
-    EfficiencyDataDetail, EfficiencyTransaction)
+    EfficiencyDataDetail,
+    EfficiencyTransaction,
+)
 from flask_restful import Resource
 from flask_restful.reqparse import Argument
 
@@ -20,6 +22,8 @@ data_schema_with_rel = EfficiencyTransactionSchema()
 data_repository = DataRepository(EfficiencyTransaction)
 
 from digital_twin_migration.models.pfi_app import PFICategory, PFIEquipment
+
+
 class DataListResource(Resource):
     """
     Resource for retrieving and creating Transactions.
@@ -32,16 +36,25 @@ class DataListResource(Resource):
         Argument("all", location="args", type=bool, required=False, default=False),
         Argument("start_date", location="args", type=str, required=False, default=None),
         Argument("end_date", location="args", type=str, required=False, default=None),
-        Argument("is_performance_test", location="args", type=bool, required=False, default=False),
+        Argument(
+            "is_performance_test",
+            location="args",
+            type=bool,
+            required=False,
+            default=False,
+        ),
     )
     def get(self, user_id, page, size, all, start_date, end_date, is_performance_test):
         test = PFICategory.query.all()
-        
-        
+
         # Apply pagination
-        data = data_controller.paginated_list_data(
-            page, size, all, start_date, end_date
-        ) if not is_performance_test else data_controller.paginated_performance_test_data(page, size, all, start_date, end_date)
+        data = (
+            data_controller.paginated_list_data(page, size, all, start_date, end_date)
+            if not is_performance_test
+            else data_controller.paginated_performance_test_data(
+                page, size, start_date, end_date
+            )
+        )
         return response(
             200,
             True,
@@ -74,14 +87,40 @@ class DataListResource(Resource):
         ),
         Argument("excel_id", location="json", required=True, type=str),
         Argument("inputs", location="json", required=True, type=dict),
-        Argument("is_performance_test", location="json", required=False, type=bool, default=False),
-        Argument("performance_test_weight", location="json", required=False, type=int, default=100),
+        Argument(
+            "is_performance_test",
+            location="json",
+            required=False,
+            type=bool,
+            default=False,
+        ),
+        Argument(
+            "performance_test_weight",
+            location="json",
+            required=False,
+            type=int,
+            default=100,
+        ),
     )
     @token_required
-    def post(self, jenis_parameter, excel_id, inputs, user_id, name, is_performance_test, performance_test_weight):
-
+    def post(
+        self,
+        jenis_parameter,
+        excel_id,
+        inputs,
+        user_id,
+        name,
+        is_performance_test,
+        performance_test_weight,
+    ):
         data = data_controller.create_data(
-            jenis_parameter, excel_id, inputs, user_id, name, is_performance_test, performance_test_weight
+            jenis_parameter,
+            excel_id,
+            inputs,
+            user_id,
+            name,
+            is_performance_test,
+            performance_test_weight,
         )
 
         return response(
@@ -93,8 +132,7 @@ class DataListResource(Resource):
 
 
 class DataResource(Resource):
-
-    @ token_required
+    @token_required
     def get(self, transaction_id, user_id):
         if not transaction_id:
             return response(400, False, "Transaction ID is required")
@@ -114,18 +152,22 @@ class DataResource(Resource):
             data_schema_with_rel.dump(transaction),
         )
 
-    @ token_required
+    @token_required
     def delete(self, user_id, transaction_id):
         data = data_controller.delete_data(transaction_id)
 
-        return response(200, True, "Transaction deleted successfully", data_schema.dump(data))
+        return response(
+            200, True, "Transaction deleted successfully", data_schema.dump(data)
+        )
 
-    @ token_required
-    @ parse_params(
+    @token_required
+    @parse_params(
         Argument("inputs", type=dict, required=True),
         Argument("name", type=str, required=False, default=None),
     )
     def put(self, transaction_id, user_id, inputs, name=None):
         data = data_controller.update_data(transaction_id, user_id, inputs, name)
 
-        return response(200, True, "Transaction updated successfully", data_schema.dump(data))
+        return response(
+            200, True, "Transaction updated successfully", data_schema.dump(data)
+        )
