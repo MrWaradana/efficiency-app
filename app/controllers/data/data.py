cf22ed5f08990f452a136a720affafdbc35e64b1
Cizz22 <cisatraa@gmail.com>
2024-10-11 13:24:59 +0700

    fix cache

--- app/controllers/data/data.py
+++ app/controllers/data/data.py
@@ -313,6 +313,8 @@ class DataController(BaseController[EfficiencyTransaction]):
         data_repository.create_bulk(transaction_records)
         data_repository.update_thermoflow_status(False)
         transaction.status = "Done"
+        
+        cache_flask.delete("get_data_paginated")
 
         sse.publish({"message": "Output has been processed", "status": True}, type="data_outputs")
 
